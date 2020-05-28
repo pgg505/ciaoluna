365eb20069200f304690965676e4a228b9ca031b
Peter Giordano <peter.giordano@york.ac.uk>
2020-05-28 19:52:27 +0100

    Tests: add another test for sorting function.

--- spec/ciaoluna_spec.rb
+++ spec/ciaoluna_spec.rb
@@ -1,3 +1,6 @@
+require 'italian/ruby'
+
+# Proviamo il codice del modulo invece dal gemma.
 require '../lib/ciaoluna.rb'
 
 describe 'suddividi_le_traduzioni_usando_punti' do
@@ -13,6 +16,24 @@ describe 'suddividi_le_traduzioni_usando_punti' do
         ["Secondo", "Second", "1"],
         ["Terzo", "Third", "2"],
         ["Primo", "First", "3"]
+      ]
+
+      expect(Ciaoluna.suddividi_le_traduzioni_usando_punti(traduzioni)).to eq soluzione
+    end
+  end
+
+  context 'con traduzioni in ordine casuale ma due hanno il stesso numero di punti' do
+    it 'suddivide le traduzioni usando punti' do
+      traduzioni = [
+        ["Primo", "First", "0"],
+        ["Secondo", "Second", "1"],
+        ["Terzo", "Third", "0"]
+      ]
+
+      soluzione = [
+        ["Primo", "First", "0"],
+        ["Terzo", "Third", "0"],
+        ["Secondo", "Second", "1"]
       ]
 
       expect(Ciaoluna.suddividi_le_traduzioni_usando_punti(traduzioni)).to eq soluzione
