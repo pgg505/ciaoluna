8a3c1cb6ade69413f891c0500958b4609acbfa20
Peter Giordano <peter.giordano@york.ac.uk>
2020-05-28 23:39:11 +0100

    Tests: add another one.

--- spec/ciaoluna_spec.rb
+++ spec/ciaoluna_spec.rb
@@ -22,6 +22,24 @@ describe 'suddividi_le_traduzioni_usando_punti' do
     end
   end
 
+  context 'con traduzioni in ordine casuale ma hanno il stesso numero di punti' do
+    it 'suddivide le traduzioni usando punti' do
+      traduzioni = [
+        ["Primo", "First", "0"],
+        ["Secondo", "Second", "0"],
+        ["Terzo", "Third", "0"]
+      ]
+
+      soluzione = [
+        ["Primo", "First", "0"],
+        ["Secondo", "Second", "0"],
+        ["Terzo", "Third", "0"]
+      ]
+
+      expect(Ciaoluna.suddividi_le_traduzioni_usando_punti(traduzioni)).to eq soluzione
+    end
+  end
+
   context 'con traduzioni in ordine casuale ma due hanno il stesso numero di punti' do
     it 'suddivide le traduzioni usando punti' do
       traduzioni = [
